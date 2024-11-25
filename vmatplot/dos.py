1c3f6351b40b26993ee356a63c3f14ea27fa9aa5
Photonico <LukeNiu@outlook.com>
2024-11-25 16:30:03 +1100

    save

--- vmatplot/dos.py
+++ vmatplot/dos.py
@@ -125,57 +125,6 @@ def extract_dos(directory_path):
             energy_dos_shift, total_dos_list, integrated_dos_list)                      # 5 ~ 7
 
 # DoS Plotting
-def plot_dos_sol(matter, x_range=None, y_top=None, dos_type=None, dos_data=None, color_family="blue"):
-    # Help information
-    help_info = "Usage: plot_dos_sol \n" + \
-                "Use extract_dos to extract the DoS data."
-
-    if matter in ["help", "Help"]:
-        print(help_info)
-        return
-
-    # Figure Settings
-    fig_setting = canvas_setting()
-    plt.figure(figsize=fig_setting[0], dpi = fig_setting[1])
-    params = fig_setting[2]; plt.rcParams.update(params)
-    plt.tick_params(direction="in", which="both", top=True, right=True, bottom=True, left=True)
-
-    # Color calling
-    fermi_color = color_sampling("Violet")
-
-    # Data plotting range
-    # y_axis_top = max(dos_data[6]); y_limit = y_axis_top * 0.6
-    # y_axis_top = max(max(total_dos_list), max(integrated_dos_list))
-    y_limit = y_top
-
-    # Data plotting
-    colors = color_sampling(color_family)
-    if dos_type in ["All", "all"]:
-        plt.plot(dos_data[5], dos_data[6], c=colors[1], label="Total DoS", zorder=3)
-        plt.plot(dos_data[5], dos_data[7], c=colors[2], label="Integrated DoS", zorder=2)
-    if dos_type in ["Total", "total"]:
-        plt.plot(dos_data[5], dos_data[6], c=colors[1], label="Total DoS", zorder=2)
-    if dos_type in ["Integrated", "integrated"]:
-        plt.plot(dos_data[5], dos_data[7], c=colors[2], label="Integrated DoS", zorder=2)
-
-    # Plot Fermi energy as a vertical line
-    efermi = dos_data[0]
-    shift = efermi
-    plt.axvline(x = efermi-shift, linestyle="--", c=fermi_color[0], alpha=1.00, label="Fermi energy", zorder=1)
-    fermi_energy_text = f"Fermi energy\n{efermi:.3f} (eV)"
-    plt.text(efermi-shift-x_range*0.02, y_limit*0.98, fermi_energy_text, fontsize =1.0*12, c=fermi_color[0], rotation=0, va = "top", ha="right")
-
-    # Title
-    # plt.title(f"Electronic density of state for {matter} ({supplement})")
-    plt.title(f"DoS {matter}")
-    plt.ylabel(r"Density of States")
-    plt.xlabel(r"Energy (eV)")
-
-    plt.ylim(0, y_limit)
-    plt.xlim(x_range*(-1), x_range)
-    plt.legend(loc="upper right")
-    plt.tight_layout()
-
 def create_matters_dos(matters_list):
     # matters = create_matters_dos(matters_list)
     # matters[0] = label
@@ -183,84 +132,36 @@ def create_matters_dos(matters_list):
     # matters[1][6] = Total DoS
     # matters[1][7] = integral dos
     # matters[2] = color family
-    # matters[3] = alpha
-    # matters[4] = linestyle
+    # matters[3] = line style
+    # matters[4] = line weight
+    # matters[5] = alpha
     matters = []
     for matter_dir in matters_list:
         if len(matter_dir) == 2:
             label, directory = matter_dir
-            color = "blue"
-            alpha = 1.0
-            linestyle = "solid"
+            line_color = "default"
+            line_style = "solid"
+            line_weight = 1.5
+            line_alpha = 1.0
         elif len(matter_dir) == 3:
-            label, directory, color = matter_dir
-            alpha = 1.0
-            linestyle = "solid"
+            label, directory, line_color = matter_dir
+            line_style = "solid"
+            line_weight = 1.5
+            line_alpha = 1.0
         elif len(matter_dir) == 4:
-            label, directory, color, alpha = matter_dir
-            linestyle = "solid"
+            label, directory, line_color, line_style = matter_dir
+            line_weight = 1.5
+            line_alpha = 1.0
+        elif len(matter_dir) == 5:
+            label, directory, line_color, line_style, line_weight = matter_dir
+            line_alpha = 1.0
         else:
-            label, directory, color, alpha, linestyle = matter_dir
+            label, directory, line_color, line_style, line_weight, line_alpha = matter_dir
         dos_data = extract_dos(directory)
-        matters.append([label, dos_data, color, alpha, linestyle])
+        matters.append([label, dos_data,  line_color, line_style, line_weight, line_alpha])
     return matters
 
 # Universal DoS Plotting
-def plot_dos_data(title, x_range = None, y_top = None, dos_type = None, matters = None):
-    # Help information
-    help_info = "Usage: plot_dos \n" + \
-                "Use extract_dos to extract the DoS data into a two-dimensional list firstly.\n"
-
-    if title in ["help", "Help"]:
-        print(help_info)
-        return
-
-    # Figure Settings
-    fig_setting = canvas_setting()
-    plt.figure(figsize=fig_setting[0], dpi = fig_setting[1])
-    params = fig_setting[2]; plt.rcParams.update(params)
-    plt.tick_params(direction="in", which="both", top=True, right=True, bottom=True, left=True)
-
-    # Color calling
-    fermi_color = color_sampling("Violet")
-
-    if all(term is not None for term in [x_range, y_top]):
-        # Data plotting
-        if dos_type in ["All", "all"]:
-            for index, matter in enumerate(matters):
-                # Labels
-                current_label = matter[0]
-                plt.plot(matter[1][5], matter[1][6], c=color_sampling(matter[2])[1], label=f"Total DoS {current_label}", zorder = 3)
-                plt.plot(matter[1][5], matter[1][7], c=color_sampling(matter[2])[2], label=f"Integrated DoS {current_label}", zorder = 2)
-                efermi = matter[1][0]
-        if dos_type in ["Total", "total"]:
-            for index, matter in enumerate(matters):
-                # Labels
-                current_label = matter[0]
-                plt.plot(matter[1][5], matter[1][6], c=color_sampling(matter[2])[1], label=f"Total DoS {current_label}", zorder = 2)
-                efermi = matter[1][0]
-        if dos_type in ["Integrated", "integrated"]:
-            for index, matter in enumerate(matters):
-                # Labels
-                current_label = matter[0]
-                plt.plot(matter[1][5], matter[1][7], c=color_sampling(matter[2])[2], label=f"Integrated DoS {current_label}", zorder = 2)
-                efermi = matter[1][0]
-        # Plot Fermi energy as a vertical line
-        shift = efermi
-        plt.axvline(x = efermi-shift, linestyle="--", c=fermi_color[0], alpha=1.00, label="Fermi energy", zorder = 1)
-        fermi_energy_text = f"Fermi energy\n{efermi:.3f} (eV)"
-        plt.text(efermi-shift-x_range*0.02, y_top*0.98, fermi_energy_text, fontsize =1.0*12, c=fermi_color[0], rotation=0, va = "top", ha="right")
-
-        # Title
-        # plt.title(f"Electronic density of state for {title} ({supplement})")
-        plt.title(f"DoS {title}")
-        plt.ylabel(r"Density of States"); plt.xlabel(r"Energy (eV)")
-
-        plt.ylim(0, y_top)
-        plt.xlim(x_range*(-1), x_range)
-        # plt.legend(loc="best")
-        plt.legend(loc="upper right")
-
 def plot_dos(title, x_range = None, y_top = None, dos_type = None, matters_list = None):
     # Help information
     help_info = "Usage: plot_dos \n" + \
