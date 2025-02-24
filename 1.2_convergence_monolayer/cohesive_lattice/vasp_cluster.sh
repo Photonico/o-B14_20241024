de3ec1d5c2b1135eca8f7aae297b6a016fbf7e76
Photonico <LukeNiu@outlook.com>
2025-02-24 17:03:54 +1100

    save

--- 1.2_convergence_monolayer/cohesive_lattice/vasp_cluster.sh
+++ 1.2_convergence_monolayer/cohesive_lattice/vasp_cluster.sh
@@ -2,7 +2,7 @@
 #PBS -N lattice
 #PBS -q cmt
 #PBS -j oe
-#PBS -l select=1:ncpus=42:mpiprocs=42:mem=200GB
+#PBS -l select=1:ncpus=8:mpiprocs=8:mem=16GB
 #PBS -l walltime=48:00:00
 #PBS -m a
 #PBS -M luke.niu@sydney.edu.au
@@ -18,4 +18,4 @@ module load hdf/5/1.14.1-2_intel2021
 set VASP=/cmt2/ocon2505/VASP/vasp.6.5.0/bin/vasp_std
 set BIN=/cmt2/ocon2505/VASP/vasp.6.5.0/bin/vasp_std
 
-mpirun -np 42 $VASP > vasp_physics_cluster.out
+mpirun -np 8 $VASP > vasp_physics_cluster.out
