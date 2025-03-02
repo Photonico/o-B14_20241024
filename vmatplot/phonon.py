875df1324444ce3ea8388b284fb14bf9d65ede2a
Photonico <LukeNiu@outlook.com>
2025-03-02 23:17:51 +1100

    save

--- vmatplot/phonon.py
+++ vmatplot/phonon.py
@@ -13,7 +13,7 @@ from vmatplot.output_settings import color_sampling, canvas_setting
 from vmatplot.algorithms import transpose_matrix
 from vmatplot.commons import extract_fermi, get_atoms_count, process_boundary, get_or_default
 
-from vmatplot.bandstructure import extract_high_sym_details, extract_kpath, kpoints_index, kpoints_path, high_symmetry_path, is_kpoints_returning
+from vmatplot.bandstructure import extract_kpath, kpoints_index, kpoints_path, high_symmetry_path, is_kpoints_returning
 from vmatplot.dos import extract_dos
 from vmatplot.pdos import extract_dict_pdos, create_matters_pdos
 
