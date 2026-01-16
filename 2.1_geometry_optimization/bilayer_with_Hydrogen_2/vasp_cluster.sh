d35ca3c6846868ec308eff1ec174802a7bdb7978
Photonico <LukeNiu@outlook.com>
2026-01-16 14:54:12 +1100

    update

--- 2.1_geometry_optimization/bilayer_with_Hydrogen_2/vasp_cluster.sh
+++ 2.1_geometry_optimization/bilayer_with_Hydrogen_2/vasp_cluster.sh
@@ -1,8 +1,8 @@
 #!/bin/csh
-#PBS -N phonon
+#PBS -N lattice
 #PBS -q cmt
 #PBS -j oe
-#PBS -l select=1:ncpus=168:mpiprocs=168:mem=1000GB
+#PBS -l select=1:ncpus=42:mpiprocs=42:mem=200GB
 #PBS -l walltime=48:00:00
 #PBS -m a
 #PBS -M luke.niu@sydney.edu.au
@@ -18,4 +18,4 @@ module load hdf/5/1.14.1-2_intel2021
 set VASP=/cmt2/ocon2505/VASP/vasp.6.5.0/bin/vasp_std
 set BIN=/cmt2/ocon2505/VASP/vasp.6.5.0/bin/vasp_std
 
-mpirun -np 168 $VASP > vasp_physics_cluster.out
+mpirun -np 42 $VASP > vasp_physics_cluster.out
